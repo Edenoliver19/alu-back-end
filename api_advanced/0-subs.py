4881c66278af364a5cc37d90708cd7dcfa14684f
eden <o.migisha@alustudent.com>
2026-02-23 10:37:07 -0800

    Task 0: Get number of subreddit subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,26 +7,27 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns number of subscribers, 0 if invalid subreddit"""
+    """Returns number of subscribers, 0 if invalid"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {
-        "User-Agent": "linux:alu-scripting:student:v1.0 (by /u/anonymous)"
+        "User-Agent": "linux:alu-project:v1.0 (by /u/anonymous)"
     }
 
     try:
         response = requests.get(
             url,
             headers=headers,
-            allow_redirects=False
+            allow_redirects=False,
+            timeout=10
         )
+
 
         if response.status_code != 200:
             return 0
 
-        results = response.json()
-        return results.get("data").get("subscribers")
+        return response.json().get("data", {}).get("subscribers", 0)
 
     except Exception:
         return 0
